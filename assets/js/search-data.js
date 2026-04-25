107d7190341019f874fff3466764e78a39699a1d
SkyWatcher-123 <112517055+SkyWatcher-123@users.noreply.github.com>
2026-04-25 23:59:40 +0000

    Deploying to gh-pages from @ SkyWatcher-123/SkyWatcher-123.github.io@d774e56f212d25bbd24827b60206a0990c75d8d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,49 +416,49 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-robotic-plastering-with-diffusion-policy",
+          title: 'Robotic Plastering with diffusion Policy',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-multi-robot-system-for-construction",
+          title: 'Multi-Robot System for Construction',
+          description: "my on-going PhD thesis",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-assembly-and-disassembly",
+          title: 'Assembly and Disassembly',
+          description: "Tag Matters",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-perceptive-drawing",
+          title: 'perceptive drawing',
+          description: "ITAR Assignment",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-rethinking-automation-in-construction",
+          title: 'Rethinking Automation in Construction',
+          description: "human campanion roaming rover in construction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-multi-robot-assembly",
+          title: 'Multi-robot assembly',
+          description: "FEA for structure assembly",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-color-mixing",
+          title: 'Color Mixing',
+          description: "machine learning for color-mixing",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+            },},{id: "projects-air-drawing",
+          title: 'Air Drawing',
+          description: "Selected student project for ARWP",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
+            },},{id: "projects-waste-assembly",
+          title: 'waste assembly',
+          description: "selected student prject for ARWP",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{id: "teachings-f25-introduction-to-architectural-robotics",
